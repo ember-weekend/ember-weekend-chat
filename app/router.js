06f13ab918d34f6e70b2ff70b67832c2e0bfb904
Chase McCarthy <chase@code0100fun.com>
2016-05-24 06:24:29 -0700

    Add chat route

--- app/router.js
+++ app/router.js
@@ -8,6 +8,9 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('sign-in');
   this.authenticatedRoute('home', { path: '/' });
+  this.route('chat', function() {
+    this.route('show', { path: '/:room' });
+  });
 });
 
 export default Router;
